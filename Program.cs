b2e608f4a6d5baaf5c81a0eeec5e41afbdb6fdd9
akaMaxg <m.guclu@live.se>
2022-12-21 10:38:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fullstack_NET_MaxG_ChasAcademy22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
